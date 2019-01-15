18d667cacb89ecbf93d2aff5eb80d443e2be83f5
lingchuanA <875321727@qq.com>
2019-01-15 15:26:16 +0800

    爬虫日志

--- src/main/java/com/robot/util/LogHelper.java
+++ src/main/java/com/robot/util/LogHelper.java
@@ -1,8 +1,10 @@
 package com.robot.util;
 
-import org.apache.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LogHelper {
 
-    public static final Logger scheduleTaskLog = Logger.getLogger("scheduleTaskLogger");
+    public static final Logger scheduleTaskLog = LogManager.getLogger("scheduleTask");
 }
