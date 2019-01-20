9b08789610ad6eaf509f7d4dbbb936359a2bb81b
张瑞钧 <3117002462@mail2.gdut.edu.cn>
2019-01-20 17:59:19 +0800

    where are my changes?!!!

--- src/main/java/com/robot/service/CommonService.java
+++ src/main/java/com/robot/service/CommonService.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author asce
@@ -49,42 +50,116 @@ public class CommonService {
      * @param
      * @return
      */
-    public String getIndex(){
+    public String getIndex() throws InterruptedException{
         Map<String,Object> dataMap = new HashMap<>();
         //原机器人协会
-        dataMap.put("news",informationService.getIndexAssociationNews());
-        dataMap.put("members",introductionService.getIndexMember());
-        dataMap.put("notices",informationService.getIndexNotice());
-        dataMap.put("information1",informationService.findInformationTop());
-        dataMap.put("policy",informationService.findPolicyTop());
-        dataMap.put("school",introductionService.getIndexUniversity());
-        dataMap.put("experts",introductionService.getIndexExpert());
-        dataMap.put("expertArts",informationService.getIndexExpertArt());
-        //企业
-        dataMap.put("companyNews", informationService.getCompanyNews());
-        dataMap.put("companyBrand", companyService.getCompanyBrand());
-        dataMap.put("companyDynamics", informationService.getCompanyDynamics());
-        //资讯
-        dataMap.put("hotspot",informationService.getIndexHot());
-        dataMap.put("report",informationService.findReportTop());
-        //技术
-        //dataMap.put("case",technologyService.getIndexCase());
-        dataMap.put("basic",informationService.getIndexBasic());
-        dataMap.put("science",informationService.getIndexDiscuss());
-        dataMap.put("educationTrain",informationService.getEducationTrain());
-        //产品
-        dataMap.put("productEvaluation", informationService.getIndexEvaluate());
-        dataMap.put("productLibrary", productService.getProductLibrary());
-        dataMap.put("productNews",informationService.getIndexProductNews());
-        dataMap.put("productRecommend",informationService.getIndexRecommend());
-        //招聘
-        dataMap.put("position", positionService.getIndex());
-        //会议
-        dataMap.put("conference",conferenceService.getIndexConference());
-        dataMap.put("metting", conferenceService.getIndexMetting());
-        //相关热点
-        dataMap.put("relatedHot",informationService.findIndexRelatedHot());
+        CommonUtil.getTime(null);
+//        dataMap.put("news",informationService.getIndexAssociationNews());
+//        dataMap.put("members",introductionService.getIndexMember());
+//        dataMap.put("notices",informationService.getIndexNotice());
+//        dataMap.put("information1",informationService.findInformationTop());
+//        dataMap.put("policy",informationService.findPolicyTop());
+//        dataMap.put("school",introductionService.getIndexUniversity());
+//        dataMap.put("experts",introductionService.getIndexExpert());
+//        dataMap.put("expertArts",informationService.getIndexExpertArt());
+//        //企业
+//        dataMap.put("companyNews", informationService.getCompanyNews());
+//        dataMap.put("companyBrand", companyService.getCompanyBrand());
+//        dataMap.put("companyDynamics", informationService.getCompanyDynamics());
+//        //资讯
+//        dataMap.put("hotspot",informationService.getIndexHot());
+//        dataMap.put("report",informationService.findReportTop());
+//        //技术
+//        //dataMap.put("case",technologyService.getIndexCase());
+//        dataMap.put("basic",informationService.getIndexBasic());
+//        dataMap.put("science",informationService.getIndexDiscuss());
+//        dataMap.put("educationTrain",informationService.getEducationTrain());
+//        //产品
+//        dataMap.put("productEvaluation", informationService.getIndexEvaluate());
+//        dataMap.put("productLibrary", productService.getProductLibrary());
+//        dataMap.put("productNews",informationService.getIndexProductNews());
+//        dataMap.put("productRecommend",informationService.getIndexRecommend());
+//        //招聘
+//        dataMap.put("position", positionService.getIndex());
+//        //会议
+//        dataMap.put("conference",conferenceService.getIndexConference());
+//        dataMap.put("metting", conferenceService.getIndexMetting());
+//        //相关热点
+//        dataMap.put("relatedHot",informationService.findIndexRelatedHot());
+        final CountDownLatch countDownLatch = new CountDownLatch(4);
+        new Thread(()->run1(dataMap,countDownLatch)).start();
+        new Thread(()->run2(dataMap,countDownLatch)).start();
+        new Thread(()->run3(dataMap,countDownLatch)).start();
+        new Thread(()->run4(dataMap,countDownLatch)).start();
+        countDownLatch.await();
+        CommonUtil.getTime(null);
         return GsonUtil.getSuccessJson(dataMap);
+    }
+
+    private void run1(Map<String,Object> dataMap,CountDownLatch countDownLatch){
+        CommonUtil.getTime(1);
+        try {
+            dataMap.put("news",informationService.getIndexAssociationNews());
+            dataMap.put("members",introductionService.getIndexMember());
+            dataMap.put("notices",informationService.getIndexNotice());
+            dataMap.put("expertArts",informationService.getIndexExpertArt());
+        } finally {
+            countDownLatch.countDown();
+        }
+        CommonUtil.getTime(1);
+    }
+
+    private void run2(Map<String,Object> dataMap,CountDownLatch countDownLatch){
+        CommonUtil.getTime(2);
+        try {
+            dataMap.put("information1",informationService.findInformationTop());
+            dataMap.put("policy",informationService.findPolicyTop());
+            dataMap.put("expertArts",informationService.getIndexExpertArt());
+        } finally {
+            countDownLatch.countDown();
+        }
+        CommonUtil.getTime(2);
+    }
+
+    private void run3(Map<String,Object> dataMap,CountDownLatch countDownLatch){
+        CommonUtil.getTime(3);
+        try {
+            dataMap.put("companyNews", informationService.getCompanyNews());
+            dataMap.put("companyBrand", companyService.getCompanyBrand());
+            dataMap.put("companyDynamics", informationService.getCompanyDynamics());
+            //资讯
+            dataMap.put("hotspot",informationService.getIndexHot());
+            dataMap.put("report",informationService.findReportTop());
+            //技术
+            //dataMap.put("case",technologyService.getIndexCase());
+            dataMap.put("basic",informationService.getIndexBasic());
+            dataMap.put("science",informationService.getIndexDiscuss());
+            dataMap.put("educationTrain",informationService.getEducationTrain());
+        } finally {
+            countDownLatch.countDown();
+        }
+        CommonUtil.getTime(3);
+    }
+
+    private void run4(Map<String,Object> dataMap,CountDownLatch countDownLatch){
+        CommonUtil.getTime(4);
+        //产品
+        try {
+            dataMap.put("productEvaluation", informationService.getIndexEvaluate());
+            dataMap.put("productLibrary", productService.getProductLibrary());
+            dataMap.put("productNews",informationService.getIndexProductNews());
+            dataMap.put("productRecommend",informationService.getIndexRecommend());
+            //招聘
+            dataMap.put("position", positionService.getIndex());
+            //会议
+            dataMap.put("conference",conferenceService.getIndexConference());
+            dataMap.put("metting", conferenceService.getIndexMetting());
+            //相关热点
+            dataMap.put("relatedHot",informationService.findIndexRelatedHot());
+        } finally {
+            countDownLatch.countDown();
+        }
+        CommonUtil.getTime(4);
     }
 
     /**
@@ -153,7 +228,7 @@ public class CommonService {
         return GsonUtil.getErrorJson("上传失败");
     }
 
-     /**
+    /**
      * 上传文件
      * @author asce
      * @date 2018/11/30
